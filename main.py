4056ca6d2abaae6fffd4c849307789967a2cb455
carlosomar <carlosodmen@gmail.com>
2023-02-27 10:23:46 -0400

    criando csv

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from lib2to3.pgen2 import driver
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 import time
 import csv
+
+
+
 
 options = webdriver.ChromeOptions()
 
@@ -11,8 +17,11 @@ navegador = webdriver.Chrome(chrome_options=options)
 
 navegador.get("https://asesweb.governoeletronico.gov.br")
 
-urls = ['www.tjro.jus.br/', 'https://www.tjro.jus.br/resp-institucional/resp-conheca-pj',
+urls = ['www.tjro.jus.br/','https://www.tjro.jus.br/resp-institucional/resp-conheca-pj',
         'https://www.tjro.jus.br/resp-institucional/resp-conheca-pj']
+
+templist = []
+
 for url in urls:
 
     navegador.find_element(By.ID, 'url').send_keys(url)
@@ -37,26 +46,37 @@ for url in urls:
         d = []
         for j in range(1, cols + 1):
             d.append(navegador.find_element(By.XPATH,"//*[@id='tabelaErros']/tbody/tr[" + str(i) + "]/td[" + str(j) + "]").text)
-        resultTab.append({"secao": d[0], "erros": d[1], "avisos": d[2]})
+        resultTab.append({ d[0]+ " erros": d[1], "avisos": d[2]})
+
+
 
 
     resultTotal = []
+
     for i in range(1, row_total + 1 ):
         d = []
         for j in range(1, cols_total + 1):
             d.append(navegador.find_element(By.XPATH,"//*[@id='tabelaErros']/tfoot/tr[" + str(i) + "]/td[" + str(j) + "]").text)
-        resultTotal.append({"Total": d[0],"erros": d[1],"aviso": d[2]})
+        resultTotal.append({d[0]+ " erros": d[1],"aviso": d[2]})
 
     percent = []
     for p in range(1, percentage +1):
         percent.append(navegador.find_element(By.XPATH,"//*[@id='webaxscore']/span["+str(p)+"]").text)
-    percent.append({percent[0]})
+    percent[0]
 
 
 
     print(resultTab)
     print(resultTotal)
     print(percent)
+
+
+    templist.append(resultTab)
+    templist.append(resultTotal)
+    templist.append(percent)
+    df = pd.DataFrame(templist)
+    df.to_csv('table.xlsx')
+    driver.close()
 
 
 
